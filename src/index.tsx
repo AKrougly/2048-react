5a9b1b3ab1bedec0b243a69b849dd5cedc1cdef8
Антон Круглый <A.Krougly@besmart.by>
2025-01-20 12:15:34 +0300

    react v19

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import './index.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
